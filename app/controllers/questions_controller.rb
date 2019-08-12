dc807365fcc048e0083854a4701c4a911d4599ad
Pedro Lages <pedrocamposlages@gmail.com>
2019-08-12 18:58:21 +0100

    Added all the logic to the answer action, also and some style to the views

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @question = params[:question]
+
+    @answer = if @question.end_with?('?')
+                'Silly question, get dressed and go to work!.'
+              elsif @question.blank?
+                "I can't hear you!"
+              elsif @question == 'I am going to work'
+                'Great'
+              else
+                "I don't care, get dressed and go to work!"
+              end
+  end
+end
